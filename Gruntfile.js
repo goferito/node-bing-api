e7db6a314c9f1ae75655012a8d287826117d531a
goferito <goferito@gmail.com>
2014-10-04 23:00:16 +0200

    jshint laxbreak

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,36 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        simplemocha: {
-            options: {
-                globals: ['should'],
-                timeout: 3000,
-                ignoreLeaks: false,
-                ui: 'bdd',
-                reporter: 'list'
-            },
-            all: { src: ['test/**/*.js'] }
-        },
+  grunt.initConfig({
+    simplemocha: {
+      options: {
+        globals: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        ui: 'bdd',
+        reporter: 'list'
+      },
+      all: { src: ['test/**/*.js'] }
+    },
 
-        jshint: {
-            all: [ 'Gruntfile.js', 'index.js', 'lib/**/*.js', 'test/**/*.js']
-        },
+    jshint: {
+      options: {
+        laxbreak: true
+      },
+      all: [ 'Gruntfile.js', 'index.js', 'lib/**/*.js', 'test/**/*.js']
+    },
 
-        jsdoc: {
-            all: {
-                src: ['index.js', 'lib/**/*.js'],
-                dest: 'doc'
-            }
-        }
-    });
+    jsdoc: {
+      all: {
+        src: ['index.js', 'lib/**/*.js'],
+        dest: 'doc'
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-simple-mocha');
-    grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-jsdoc');
 
-    grunt.registerTask('test', ['simplemocha:all']);
-    grunt.registerTask('build', ['jshint', 'test']);
-    grunt.registerTask('default', ['build', 'jsdoc']);
+  grunt.registerTask('test', ['simplemocha:all']);
+  grunt.registerTask('build', ['jshint', 'test']);
+  grunt.registerTask('default', ['build', 'jsdoc']);
 };
